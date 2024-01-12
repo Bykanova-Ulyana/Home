f42010ab07d5fad8953901c650bf00f82b503351
Ulyana Bykanova <bikanova.ulyana@yandex.ru>
2024-01-12 22:20:58 +0300

    Добавлены числовые параметры

--- House/rooms/base.go
+++ House/rooms/base.go
@@ -2,19 +2,23 @@ package rooms
 
 import (
 	"golang/Home/House/appliances"
+	"golang/Home/House/crockery"
+	"golang/Home/House/family"
 	"golang/Home/House/furniture"
+	"golang/Home/House/plumbing"
+	"golang/Home/House/windows"
 )
 
-// Описываем эту квартиру: https://www.cian.ru/rent/flat/296918422/
+func (home Home) CreatHome() Home {
+	home = Home{Rooms: []Room{{
 
-func CreatHome() []Home {
-	home := []Home{{Rooms: []Room{{
-		Type:   "Коридор",
-		Width:  3.18,
-		Length: 5.94,
+		// ---------------ПРИХОЖАЯ------------------
+
+		Type:   "Прихожая",
+		Width:  3.15,
+		Length: 3.47,
 		Height: 2,
-		Area:   0,
-		Appliances: []appliances.Appliance{{
+		Appliances: []appliances.Appliance{{ // БЫТОВЫЕ ПРИБОРЫ
 			Name:   "Пылесос",
 			Brand:  "Hyundai",
 			Power:  220,
@@ -23,13 +27,154 @@ func CreatHome() []Home {
 			Height: 0.49,
 			IsOn:   false,
 		}},
-		Family:    nil,
-		Crockery:  nil,
-		Plumbings: nil,
-		Furniture: []furniture.Furniture{{
-			Type: "Кровать",
+		Furniture: []furniture.Furniture{{ // МЕБЕЛЬ
+			Type:     "Прихожая",
+			Number:   1,
+			Height:   0.19,
+			Width:    0.4,
+			Length:   1,
+			Color:    "белый",
+			Material: "ЛДСП",
+		}, {
+			Type:     "Зеркало",
+			Number:   1,
+			Height:   0.9,
+			Width:    0.6,
+			Length:   0.05,
+			Color:    "белый",
+			Material: "ЛДСП",
+		}, {
+			Type:     "Когтеточка",
+			Number:   1,
+			Height:   1.5,
+			Width:    0.3,
+			Length:   0.5,
+			Color:    "бежевый",
+			Material: "дерево",
 		}},
-		Pets: nil,
-	}}}}
+		Pets: []family.Pets{{ // ЖИВОТНЫЕ
+			TypeAnimal:     "кошка",
+			Breed:          "нет",
+			Name:           "Мурка",
+			Colour:         "серый",
+			Weight:         3.3,
+			SexAnimal:      true,
+			AllergicFactor: "шерсть",
+		}},
+	}, {
+
+		//------------------СПАЛЬНЯ----------------------
+
+		Type:   "Спальня",
+		Width:  3.18,
+		Length: 5.94,
+		Height: 2,
+		Appliances: []appliances.Appliance{{ // МЕБЕЛЬ
+			Name:   "Телевизор",
+			Brand:  "Samsung",
+			Power:  69,
+			Width:  0.071,
+			Length: 0.72,
+			Height: 0.42,
+			IsOn:   true,
+		}, {
+			Name:   "Торшер",
+			Brand:  "Ambrella",
+			Power:  55,
+			Width:  0.84,
+			Length: 1.7,
+			Height: 0.84,
+			IsOn:   true,
+		}, {
+			Name:   "Вентилятор",
+			Brand:  "Aceline",
+			Power:  55,
+			Width:  0.44,
+			Length: 1.3,
+			Height: 0.39,
+			IsOn:   false,
+		}},
+		Family: []family.FamilyMember{{
+			Name:       "Анна",
+			Surname:    "Быканова",
+			Patronymic: "Николаевна",
+			Relation:   "мать",
+			Birthday:   "21.03.1978",
+			Gender:     "ж",
+			Allergy:    "нет",
+		}, {
+			Name:       "Федор",
+			Surname:    "Быканов",
+			Patronymic: "Иванович",
+			Relation:   "отец",
+			Birthday:   "26.04.1975",
+			Gender:     "м",
+			Allergy:    "нет",
+		}},
+		Crockery: []crockery.Crockery{{
+			Type:      "Чайная пара",
+			Purpose:   "сервировка стола",
+			Number:    6,
+			Material:  "фарфор",
+			Engraving: false,
+			Capacity:  250,
+		}},
+		Furniture: []furniture.Furniture{{
+			Type:     "Диван",
+			Number:   1,
+			Height:   0.9,
+			Width:    1.55,
+			Length:   2.41,
+			Color:    "тёмно-серый",
+			Material: "ППУ, ДСП",
+		}, {
+			Type:     "Шкаф",
+			Number:   1,
+			Height:   1.6,
+			Width:    0.5,
+			Length:   2.3,
+			Color:    "Венге",
+			Material: "ЛДСП, зеркало",
+		}, {
+			Type:     "Сервант",
+			Number:   2,
+			Height:   2,
+			Width:    69,
+			Length:   39.6,
+			Color:    "Венге",
+			Material: "ЛДСП, МДФ, стекло",
+		}},
+	}, {
+
+		//----------------------КУХНЯ---------------------
+		Type:       "Кухня",
+		Width:      2.1,
+		Length:     3.15,
+		Height:     2,
+		Appliances: []appliances.Appliance{{}},
+		Crockery:   []crockery.Crockery{{}},
+		Plumbings:  []plumbing.Plumbing{{}},
+		Furniture:  []furniture.Furniture{{}},
+		Window: windows.Window{
+			Number:     1,
+			Length:     1,
+			Width:      1.2,
+			OpenWindow: false,
+		},
+	}, {
+
+		//-------------------ВАННАЯ---------------------
+
+		Type:       "Ванная",
+		Width:      1.57,
+		Length:     2.07,
+		Height:     2,
+		Appliances: []appliances.Appliance{{}},
+		Family:     []family.FamilyMember{{}},
+		Crockery:   []crockery.Crockery{{}},
+		Plumbings:  []plumbing.Plumbing{{}},
+		Furniture:  []furniture.Furniture{{}},
+	}}}
+
 	return home
 }
